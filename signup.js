e580be7a5915a05f55bce6f89379a6168c9aeefa
nymaina <annmaina614@gmail.com>
2025-02-21 16:58:23 +0300

    update right container image

--- signup.js
+++ signup.js
@@ -44,7 +44,7 @@ class SignupForm extends HTMLElement {
                     }
                     .left-section {
                         width: 50%;
-                        background: url('https://source.unsplash.com/600x800/?nature') no-repeat center;
+                        background: url('https://images.unsplash.com/photo-1734531352669-11c2b2ddea3a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDExNXx8fGVufDB8fHx8fA%3D%3D') no-repeat center;
                         background-size: cover;
                         display: flex;
                         flex-direction: column;
@@ -61,7 +61,7 @@ class SignupForm extends HTMLElement {
                         padding: 10px;
                         border-radius: 5px;
                         border: 1px solid var(--input-border);
-                        background: var(--input-bg);
+                        background: var(--bd-color);
                         color: var(--text-color);
                     }
                     button {
